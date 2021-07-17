df657b04ecb63aab754ce774d77d18d7fc7e4f71
Mohamed Ibrahem <mohamed.ibrahem@yourparts.com>
2021-07-17 15:20:53 +0200

    initialize models

---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from .category import Category
+from .pet import Pet
+from .order import Order, OrderItems
---
+++ app/models/pet.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+class Pet(models.Model):
+    FEMALE = 'F'
+    MALE = 'M'
+    gender_choices = ((FEMALE, FEMALE), (MALE, MALE))
+    category = models.ForeignKey('app.Category', on_delete=models.PROTECT, related_name='category_pets')
+    name = models.CharField(max_length=50, unique=True)
+    born_date = models.DateField()
+    price = models.FloatField(default=100)
+    quantity = models.IntegerField(default=1)
+    gender = models.CharField(max_length=5, choices=gender_choices)
+
+    class Meta:
+        ordering = ['-born_date']
+
+    def __str__(self):
+        return f'{self.category.breed_name} - {self.name}'
+
+    @property
+    def age(self):
+        from datetime import datetime
+        return (datetime.now().date() - self.born_date).days
