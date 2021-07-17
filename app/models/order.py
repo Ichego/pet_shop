7fd8061f7703841c62500958eb8035da49fa024d
Ichego <38944390+Ichego@users.noreply.github.com>
2021-07-18 00:26:06 +0200

    Feature/app endpoints (#3)
    * add covrage
    
    Co-authored-by: Mohamed Ibrahem <mohamed.ibrahem@yourparts.com>

--- app/models/order.py
+++ app/models/order.py
@@ -5,6 +5,7 @@ from django.utils.functional import cached_property
 
 
 class Order(models.Model):
+    code = models.CharField(max_length=255, unique=True, null=True)
     customer = models.ForeignKey(
         User, on_delete=models.SET_NULL, related_name="user_orders", null=True
     )
@@ -14,6 +15,20 @@ class Order(models.Model):
     email = models.EmailField()
     currency = models.CharField(max_length=50, default="USD")
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.code = self.auto_generate_code()
+        super().save(*args, **kwargs)
+
+    def auto_generate_code(self):
+        import hashlib
+        import uuid
+
+        orders_no = Order.objects.all().count()
+        string_to_hash = f"{self.customer or (orders_no * -1)}.{orders_no}"
+        code = f"#{hashlib.sha256(str.encode(string_to_hash + uuid.uuid4().hex)).hexdigest()[:8]}"
+        return code
+
     @cached_property
     def total_price(self):
         from django.db.models import Sum, F
@@ -21,6 +36,17 @@ class Order(models.Model):
         return self.order_items.aggregate(total_price=Sum(F("quantity") * F("price")))[
             "total_price"
         ]
+
+    def add_items(self, items):
+        from django.shortcuts import get_object_or_404
+        from .pet import Pet
+
+        for item in items:
+            OrderItems.objects.create(
+                order=self,
+                dog=get_object_or_404(Pet, pk=item["pet"]),
+                quantity=item["quantity"],
+            )
 
 
 class OrderItems(models.Model):
@@ -30,8 +56,18 @@ class OrderItems(models.Model):
     dog = models.ForeignKey(
         "app.Pet", on_delete=models.PROTECT, related_name="user_orders"
     )
-    price = models.FloatField()
+    price = models.FloatField(default=0)
     quantity = models.IntegerField()
+
+    def save(self, *args, **kwargs):
+        self.price = self.dog.price
+        if not self.dog.quantity >= self.quantity:
+            from rest_framework.validators import ValidationError
+
+            raise ValidationError()
+        self.dog.quantity -= self.quantity
+        self.dog.save(update_fields=["quantity"])
+        super().save(*args, **kwargs)
 
     @cached_property
     def total_item_price(self):
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,87 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Pet, Category, Order
 
-# Create your tests here.
+
+class TestPetAppApi(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def add_category(self):
+        Category.objects.get_or_create(breed_name="German Shepherd")
+        Category.objects.get_or_create(breed_name="Golden Retriever")
+
+    def add_pet(self):
+        pet_dict = [
+            {
+                "category": Category.objects.first(),
+                "name": "test dog",
+                "born_date": "2021-07-16",
+                "price": 100.0,
+                "quantity": 10,
+                "gender": "10",
+            },
+            {
+                "category": Category.objects.last(),
+                "name": "test dog 2 ",
+                "born_date": "2021-07-16",
+                "price": 150.0,
+                "quantity": 10,
+                "gender": "10",
+            },
+        ]
+        Pet.objects.get_or_create(**pet_dict[0])
+        Pet.objects.get_or_create(**pet_dict[1])
+
+    def test_categories(self) -> None:
+        self.add_category()
+        response = self.client.get("/api/v1/category/")
+        assert response.status_code == 200
+        assert len(response.data) == Category.objects.count()
+        assert response.data[0].get("breed_name") == "German Shepherd"
+
+        self.add_pet()
+        response = self.client.get("/api/v1/category/1/")
+        assert response.status_code == 200
+        assert len(response.data) == Pet.objects.filter(category_id=1).count()
+        assert response.data[0].get("name") == "test dog"
+
+    def test_pets(self) -> None:
+        self.add_category()
+        self.add_pet()
+
+        response = self.client.get("/api/v1/pet/")
+        assert response.status_code == 200
+        assert len(response.data) == Pet.objects.count()
+        assert response.data[0].get("name") == "test dog"
+
+        response = self.client.get("/api/v1/pet/1/")
+        assert response.status_code == 200
+        assert response.data.get("name") == "test dog"
+
+    def test_orders(self) -> None:
+        order_json = {
+            "address_one": "this is test order address",
+            "phone_number": "+201116090444",
+            "email": "a@a.com",
+            "currency": "USD",
+            "order_items": [{"pet": 1, "quantity": 2}, {"pet": 2, "quantity": 2}],
+        }
+
+        self.add_category()
+        self.add_pet()
+        response = self.client.post("/api/v1/order/", data=order_json, format="json")
+        assert response.status_code == 200
+        assert response.data.get("code") == Order.objects.last().code
+        assert response.data.get("order_items")[0].get("total_item_price") == 200.0
+        assert response.data.get("total_price") == 500.0
+        assert Pet.objects.get(pk=1).quantity == 8
+        order_json = {
+            "address_one": "this is test order address",
+            "phone_number": "+201116090444",
+            "email": "a@a.com",
+            "currency": "USD",
+            "order_items": [{"pet": 1, "quantity": 12}],
+        }
+        response = self.client.post("/api/v1/order/", data=order_json, format="json")
+        assert response.status_code == 422
